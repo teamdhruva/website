69584196546b720ccab2f54a5b200b75aff891ec
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-23 15:58:14 +0530

    Fix more layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,53 @@
-<slot></slot><script>import "../app.css";</script><style></style>
+<script>
+  import { fly } from "svelte/transition";
+  import "../app.css";
+
+  let visible = false;
+</script>
+
+<svelte:head>
+  <title>dhRuVa</title>
+
+  <script src="resp.js"></script>
+</svelte:head>
+
+<div class="flex flex-col min-h-screen">
+  <nav class="flex flex-col fixed w-full bg-black items-center justify-center">
+    <div
+      class="max-sm:hidden flex flex-row items-center text-white gap-10 bg-black max-w-screen-lg pl-2 pr-5 py-2"
+    >
+      <div class="max-sm:hidden logo">
+        <a href="#home"><img src="Dhruva logo.png" alt="logo" /></a>
+      </div>
+      <span class="flex-1"></span>
+      <div><a class="hover:text-cyan-300" href="#home">Home</a></div>
+      <div><a class="hover:text-cyan-300" href="#Projects">Projects</a></div>
+      <div><a class="hover:text-cyan-300" href="#Team">Team</a></div>
+      <div><a class="hover:text-cyan-300" href="#Alumni">Alumni</a></div>
+      <div><a class="hover:text-cyan-300" href="#Contact Us">Contact</a></div>
+    </div>
+
+    {#if visible}
+      <div
+        transition:fly={{ y: -200, duration: 300 }}
+        class="top-0 w-full fixed bg-black flex flex-col items-center gap-5 text-white p-5"
+      >
+        <div><a class="hover:text-cyan-300" on:click={() => visible=false} href="#home">Home</a></div>
+        <div><a class="hover:text-cyan-300" on:click={() => visible=false} href="#Projects">Projects</a></div>
+        <div><a class="hover:text-cyan-300" on:click={() => visible=false} href="#Team">Team</a></div>
+        <div><a class="hover:text-cyan-300" on:click={() => visible=false} href="#Alumni">Alumni</a></div>
+        <div><a class="hover:text-cyan-300" on:click={() => visible=false} href="#Contact Us">Contact</a></div>
+      </div>
+    {/if}
+
+    <button class="sm:hidden burger" on:click={() => (visible = !visible)}>
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </button>
+  </nav>
+
+  <slot />
+</div>
+
+<style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,152 +1,140 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="newstyle.css">
-    <link rel="icon" href="favicon.ico">
+<script lang="ts">
+</script>
 
-    <title>Dhruva RVCE</title>
-  </head>
-  <body>
-    <div class="flex flex-col min-h-screen">
-      <nav class="w-full navbar h-nav-resp">
-        <ul class="flex flex-row bg-black max-w-screen-lg nav-list v-class-resp">
-          <div class="logo">
-            <a href="#home"><img src="Dhruva logo.png" alt="logo"></a>
-          </div>
-          <span class="flex-1"></span>
-          <li> <a href="#home">Home</a> </li>
-          <li> <a href="#Projects">Projects</a> </li>
-          <li> <a href="#Team">Team</a> </li>
-          <li> <a href="#Alumni">Alumni</a> </li>
-          <!-- <li> <a href="#Sponsors">Sponsors</a> </li> -->
-          <li> <a href="#Contact Us">Contact</a> </li>
-    
-        </ul>
-    
-        <div class="burger">
-          <div class="line"></div>
-          <div class="line"></div>
-          <div class="line"></div>
-    
+<div class="flex flex-col min-h-screen">
+  <section class="flex flex-col background h-screen" id="home">
+    <div class="box-main max-w-[48rem] flex-1">
+      <div class="max-w-96 p-4">
+        <p class="text-big">Team Dhruva</p>
+        <p class="text-small">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type.
+        </p>
+        <div class="buttons">
+          <button class="btn-initial">
+            <a class="top1" href="https://www.youtube.com/@dhruva1910"
+              >Watch video</a
+            ></button
+          >
         </div>
-      </nav>
-       <section class="flex flex-col background h-screen" id="home">
-         <div class="box-main max-w-[48rem] flex-1">
-           <div class="max-w-96 p-4">
-           <p class="text-big">Team Dhruva</p>
-           <p class="text-small">Lorem Ipsum is simply dummy text of the printing and
-             typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type.</p>
-           <div class="buttons">
-             <button class="btn-initial"> <a  class="top1" href="https://www.youtube.com/@dhruva1910">Watch video</a></button>
-    
-    
-           </div>
-         </div>
-           <div class="secondHalf"></div>
-            <img class="space max-sm:hidden" src="newbg.jpeg" alt="telescope">
-    
-         </div>
-    
-    
-       </section>
-       <section class="secRight h-screen" id="Projects">
-         <div class="paras">
-    
-         <p class="sectionTag text-big">Projects</p>
-         <p class="flex justify-center align-middle max-w-[32rem] sectionSubTag text-small">Lorem Ipsum is simply dummy text of the printing and
-           typesetting industry.
-           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-           when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-           </div>
-           <!-- <div class="thumbnail">
+      </div>
+      <div class="secondHalf"></div>
+      <img class="space max-sm:hidden" src="newbg.jpeg" alt="telescope" />
+    </div>
+  </section>
+  <section class="secRight h-screen" id="Projects">
+    <div class="paras">
+      <p class="sectionTag text-big">Projects</p>
+      <p
+        class="flex justify-center align-middle max-w-[32rem] sectionSubTag text-small"
+      >
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+    </div>
+    <!-- <div class="thumbnail">
             <img class="thumbnail"src="newbg.jpeg" alt="sameImg">
     
            </div> -->
-    
-       </section>
-     <!-- </section> -->
-     
-     <section class="flex flex-col justify-center items-center secLeft h-screen w-full" id="Team">
-       <div>
-    
-    
-       <p class="sectionTag text-big">Team</p>
-       <div class="p-10">
-        <div class="singlepic text-white">
-          <img class="rounded-3xl max-w-60" src="img1.jpeg" alt="pic1">    
-   
-      </div>
-      <h3 class="text-white text-center">Faculty advisor:Karthik Shastry</h3>
-       </div>
+  </section>
+  <!-- </section> -->
 
-    <div class="flex flex-row justify-center items-center gap-5 flex-wrap auto-cols-max max-sm:flex-row">
-      <img class="max-w-60 max-sm:max-w-32 rounded-3xl" src="img1.jpeg" alt="pic1">
-      <img class="max-w-60 max-sm:max-w-32 rounded-3xl" src="img1.jpeg" alt="pic1">
-      <img class="max-w-60 max-sm:max-w-32 rounded-3xl" src="img1.jpeg" alt="pic1">
-      <img class="max-w-60 max-sm:max-w-32 rounded-3xl" src="img1.jpeg" alt="pic1">
+  <section
+    class="flex flex-col justify-center items-center secLeft h-screen w-full"
+    id="Team"
+  >
+    <div>
+      <p class="sectionTag text-big">Team</p>
+      <div class="p-10">
+        <div class="singlepic text-white">
+          <img class="rounded-3xl max-w-60" src="img1.jpeg" alt="pic1" />
+        </div>
+        <h3 class="text-white text-center">Faculty advisor:Karthik Shastry</h3>
+      </div>
+
+      <div
+        class="flex flex-row justify-center items-center gap-5 flex-wrap auto-cols-max max-sm:flex-row"
+      >
+        <img
+          class="max-w-60 max-sm:max-w-32 rounded-3xl"
+          src="img1.jpeg"
+          alt="pic1"
+        />
+        <img
+          class="max-w-60 max-sm:max-w-32 rounded-3xl"
+          src="img1.jpeg"
+          alt="pic1"
+        />
+        <img
+          class="max-w-60 max-sm:max-w-32 rounded-3xl"
+          src="img1.jpeg"
+          alt="pic1"
+        />
+        <img
+          class="max-w-60 max-sm:max-w-32 rounded-3xl"
+          src="img1.jpeg"
+          alt="pic1"
+        />
+      </div>
     </div>
-    
-    
-         </div>
-    
-     </section>
-    <!--</section> -->
-    <section class="secRight h-screen" id="Alumni">
-      <div class="paras">
-    
-    
+  </section>
+  <!--</section> -->
+  <section class="secRight h-screen" id="Alumni">
+    <div class="paras">
       <p class="sectionTag text-big">This is the best thing</p>
       <div class="flex flex-row overflow-hidden items-center">
-        <p class="flex-1 sectionSubTag text-small">Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        <p class="flex-1 sectionSubTag text-small">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
         </p>
         <div class="thumbnail flex-shrink">
-          <img class="secondWala max-w-10 max-sm:hidden" src="newbg.jpeg" alt="sameImg">
-        </div>
+          <img
+            class="secondWala max-w-10 max-sm:hidden"
+            src="newbg.jpeg"
+            alt="sameImg"
+          />
         </div>
       </div>
-    
-    </section>
-    <!-- </section> -->
-    <!-- <section class="secLeftLast" id="Sponsors">
+    </div>
+  </section>
+  <!-- </section> -->
+  <!-- <section class="secLeftLast" id="Sponsors">
     
         <img class="sky" src="skyroot.png" alt="gold">
     
     
     </section> -->
-    <section class="Contact" id="Contact Us">
-      <div class="Contact-Us">
-    
-        <hr>
-        <h2 class="text-centre">Contact Us</h2>
-        <br><br><br>
-        <ul class="social">
-        <li class="soc1"><a href="https://www.instagram.com/dhruva_rvce/"><h4>Instagram</h4></a> </li>
-        <li class="soc2"> <a href="#"><h4>Twitter</h4></a> </li>
-        <li class="soc3"> <a href="https://www.youtube.com/@dhruva1910"><h4>Youtube</h4></a> </li>
-        </ul>
-        <br>
-        <br>
-    
-        <h3 class="mail">Contact Us at <a href="mailto:contact@teamdhruva.com">contact@teamdhruva.com</a></h3>
-    
-        <br>
-    
-      </div>
-      <footer class="footer">
-      Copyright © Dhruva
-    
-      </footer>
-    
-    </section>
-    
-    <script class="resp.js"></script>
+  <section class="Contact" id="Contact Us">
+    <div class="Contact-Us">
+      <hr />
+      <h2 class="text-centre">Contact Us</h2>
+      <br /><br /><br />
+      <ul class="social">
+        <li class="soc1">
+          <a href="https://www.instagram.com/dhruva_rvce/"><h4>Instagram</h4></a
+          >
+        </li>
+        <li class="soc2"><a href="#"><h4>Twitter</h4></a></li>
+        <li class="soc3">
+          <a href="https://www.youtube.com/@dhruva1910"><h4>Youtube</h4></a>
+        </li>
+      </ul>
+      <br />
+      <br />
+
+      <h3 class="mail">
+        Contact Us at <a href="mailto:contact@teamdhruva.com"
+          >contact@teamdhruva.com</a
+        >
+      </h3>
+
+      <br />
     </div>
-  </body>
-</html>
+    <footer class="footer">Copyright © Dhruva</footer>
+  </section>
+</div>
