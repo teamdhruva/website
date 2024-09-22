5293a49e1a507b7991da3ed89da84bb07303e3c2
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-09-22 20:57:31 +0530

    sus

--- src/routes/(content)/projects/6-inch-telescope/+page.svelte
+++ src/routes/(content)/projects/6-inch-telescope/+page.svelte
@@ -7,15 +7,61 @@
 >
   <h1 class="text-6xl text-center font-bold text-white">6 Inch Telescope</h1>
 
-  <div class="flex flex-col max-w-screen-lg w-full gap-2">
-    <p>
-      We built a 6 inch Newtonian telescope from scratch, with a maximum
-      magnification of 300x. The telescope was built using PVC pipes and a 6
-      inch mirror. The telescope has been used to observe the moon and planets.
-    </p>
-    <p>
-      Regular sky observation sessions are conducted using the telescope. The
-      telescope is also used for astrophotography.
-    </p>
+  <div class="m-4">
+    <div class="flex flex-col max-w-screen-lg w-full gap-2">
+      <img
+        src="/projects/6-3.webp"
+        class="h-96 object-contain"
+        alt="Telescope"
+      />
+
+      <p>
+        We built a 6 inch Newtonian telescope from scratch, with a focal length
+        of 1200 mm and a maximum magnification of 300x. The telescope was built
+        using PVC pipes and a 6 inch mirror. It's not just the moon, but also
+        prominent planets like Jupiter and Saturn are a sight to behold!
+      </p>
+
+      <p>
+        This telescope - sans the readymade mirror - was made from scratch. We
+        built and assembled it using 3D printed parts and PVC pipes.
+      </p>
+
+      <hr class="mt-4 mb-4" />
+
+
+      <div
+        class="flex flex-row flex-wrap items-center justify-center gap-4 text-base"
+      >
+        <div class="flex flex-col items-center justify-center">
+          <img
+            src="/projects/6-1.webp"
+            class="h-60 object-contain"
+            alt="Telescope"
+          />
+          <p>Lunar Observation</p>
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <img
+            src="/projects/6-2.webp"
+            class="h-60 object-contain"
+            alt="Telescope"
+          />
+          <p>Solar Observation</p>
+        </div>
+      </div>
+
+      <p>
+        Regular sky observation sessions are conducted using the telescope. The
+        telescope is also used for astrophotography.
+      </p>
+
+      <p>
+        The future plan of this project is to mount it upon a fully automated
+        equatorial mount - also being developed by the team - to allow the
+        telescope to reach its full potential.
+      </p>
+    </div>
   </div>
 </div>
